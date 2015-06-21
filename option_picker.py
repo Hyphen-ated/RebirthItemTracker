6f8b1748638b05c48ebc8b0bc3cef3fdba79294c
Inschato <anydrez@gmail.com>
2015-06-20 18:39:30 -0700

    Fix arrow key item selection bug caused by floor 'items', add support for indicating or hiding rolled items

--- option_picker.py
+++ option_picker.py
@@ -122,7 +122,7 @@ for index, opt in enumerate(["background_color","text_color"]):
 
 # generate checkboxes, with special exception for show_description for message duration
 checks = {}
-for index, opt in enumerate(["show_description", "show_seed", "show_floors", "word_wrap"]):
+for index, opt in enumerate(["show_description", "show_seed", "show_floors", "show_rerolled_items", "word_wrap"]):
   checks[opt] = IntVar()
   c = Checkbutton(root, text=pretty_name(opt), variable=checks[opt])
   c.grid(row=len(entries)+1+index/2,column=index%2) # 2 checkboxes per row
