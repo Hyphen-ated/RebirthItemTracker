37885f197e48366750ec111586917f60737dda20
sillypears <stuart.rosenfeld@outlook.com>
2015-10-29 16:08:05 -0400

    Revert "Revert "Crashes on unknown Item IDs""
    This reverts commit bd9b38eb7256b6432153112da43d8c9c93676288.

--- src/item_tracker.py
+++ src/item_tracker.py
@@ -366,8 +366,12 @@ class IsaacTracker:
         return ""
 
     def get_item_info(self, item_id):
-        id_padded = item_id.zfill(3)
-        return self.items_info[id_padded]
+        print item_id
+	id_padded = item_id.zfill(3)
+	try:
+	    return self.items_info[id_padded], item_id
+	except:
+	    return self.items_info["NEW"], "NEW"
 
     def start_new_run(self, current_line_number):
         self.run_start_line = current_line_number + self.seek
@@ -532,7 +536,7 @@ class IsaacTracker:
                         space_split = line.split(" ")
                         # string has the form "Adding collectible 105 (The D6)"
                         item_id = space_split[2]
-                        item_info = self.get_item_info(item_id)
+                        item_info,item_id = self.get_item_info(item_id)
                         #If Item IDs are equal, it should say this item already exists
                         temp_item = Item(item_id,self.current_floor,item_info,getting_start_items)
                         if ((current_line_number + self.seek) - self.spawned_coop_baby) < (len(self.collected_items) + 10) \
