71220cefdc5b0137665bd7b663112cd859d66769
Hyphen-ated <Hyphen-ated@users.noreply.github.com>
2017-01-06 13:33:12 -0800

    add glow images for the new items

--- script/add_glow_effect.sh
+++ script/add_glow_effect.sh
@@ -1,5 +1,4 @@
-#Run this in 'collectibles' to resize and blur them
+#Run this in a dir with copies of new item images to add the glow effect to them in-place
 for toto in `ls *.png`; do
-    mogrify -scale 200% $toto
     convert $toto \( +clone -channel A -blur 0x2.5 -level 0,80% +channel +level-colors white \) -compose DstOver -composite $toto
 done
