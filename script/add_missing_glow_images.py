5a25025b2d6bb7fc751b0e63575a3032ce1b2d0d
Zamiel <j.nesta@gmail.com>
2017-03-15 15:18:08 -0400

    trinkets work now + more R+ custom items

--- script/add_missing_glow_images.py
+++ script/add_missing_glow_images.py
@@ -1,23 +1,26 @@
 # Run this to add glow versions of any item images that don't currently have glow versions.
-# requires imagemagick
+# This script uses the "convert" command, which is part of ImageMagick: https://www.imagemagick.org/script/download.php
 
 import os
 
 def add_glow_to_dir(dirname):
     glows = {}
-    for file in os.listdir(dirname + "/glow"):
-        if file.endswith(".png"):
+    for file in os.listdir(os.path.join(dirname, 'glow')):
+        if file.endswith('.png'):
             glows[file] = True
 
     for file in os.listdir(dirname):
         if file not in glows:
-            if file.endswith(".png"):
-                cmd = 'convert "' + dirname + '/' + file +\
-                        '" ( +clone -channel A -blur 0x2.5 -level 0,80% +channel +level-colors white ) -compose DstOver -composite "' +\
-                        dirname + '/glow/' + file + '"'
+            if file.endswith('.png'):
+                file_path = os.path.join(dirname, file)
+                file_glow_path = os.path.join(dirname, 'glow', file)
+                cmd = 'convert "' + file_path + '" ' +\
+                      '( +clone -channel A -blur 0x2.5 -level 0,80% +channel +level-colors white ) ' +\
+                      '-compose DstOver ' +\
+                      '-composite "' + file_glow_path + '"'
                 print(cmd)
                 os.system(cmd)
 
-add_glow_to_dir("../collectibles/")
-add_glow_to_dir("../collectibles/antibirth")
-add_glow_to_dir("../collectibles/custom")
+add_glow_to_dir(os.path.join('..', 'collectibles'))
+add_glow_to_dir(os.path.join('..', 'collectibles', 'antibirth'))
+add_glow_to_dir(os.path.join('..', 'collectibles', 'custom'))
--- script/rename_antibirth_images.py
+++ script/rename_antibirth_images.py
@@ -1,9 +1,10 @@
-#antibirth images come with an x at the end, but the log prints them with a 1 at the front
+# Antibirth images come with an x at the end, but the log prints them with a 1 at the front
+
 import os, re, shutil
+
 for f in os.listdir('.'):
     if os.path.isfile(f):
-        m = re.match("collectibles_(\d\d\d)x_.*\.png", f)
+        m = re.match('collectibles_(\d\d\d)x_.*\.png', f)
         if m:
             id = m.group(1)
-            shutil.move(f, "collectibles_1" + id + ".png")
-
+            shutil.move(f, 'collectibles_1' + id + '.png')
