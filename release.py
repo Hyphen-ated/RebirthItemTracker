a2dba44adc87bbb85ab9fdf51ff6639c608a9a5c
Hyphen-ated <Hyphen-ated@users.noreply.github.com>
2020-11-02 16:53:47 -0800

    document release process a little better with exact versions

--- release.py
+++ release.py
@@ -1,4 +1,4 @@
-import os, sys, shutil, subprocess
+import os, sys, shutil, subprocess, time
 
 # Here is where you can set the name for the release zip file and for the install dir inside it.
 # version.txt is the sole source of truth about what version this is. the version string shouldnt be hardcoded anywhere
@@ -9,12 +9,17 @@ installName = 'Rebirth Item Tracker'
 # target is where we assemble our final install.
 if os.path.isdir('target/'):
     shutil.rmtree('target/')
+    #windows takes time to delete things, wait until it's done
+    while os.path.isdir('target/'):
+        time.sleep(0.3)
 installDir = 'target/' + installName + '/'
 os.mkdir('target/')
 #os.mkdir(installDir)
 
+localpy = sys.executable
+
 os.chdir("src_bootstrapper")
-subprocess.call("cxfreeze.py bootstrapper.py --base-name=Win32GUI --target-dir dist --icon ../mind.ico", shell=True, stdout=sys.stdout, stderr=sys.stderr)
+subprocess.call(localpy + " cxfreeze.py bootstrapper.py --base-name=Win32GUI --target-dir dist --icon ../mind.ico", shell=False, stdout=sys.stdout, stderr=sys.stderr)
 os.chdir("..")
 
 shutil.move('src_bootstrapper/dist/bootstrapper.exe', 'src_bootstrapper/dist/Rebirth Item Tracker.exe') # Move the dist files to our target directory
@@ -22,12 +27,12 @@ shutil.move('src_bootstrapper/dist/', installDir)
 
 # Run the tracker build script. The results are placed in ./dist/
 os.chdir("src_updater")
-subprocess.call("cxfreeze.py updater.py --base-name=Win32GUI --target-dir dist --icon ../mind.ico", shell=True, stdout=sys.stdout, stderr=sys.stderr)
+subprocess.call(localpy + " cxfreeze.py updater.py --base-name=Win32GUI --target-dir dist --icon ../mind.ico", shell=False, stdout=sys.stdout, stderr=sys.stderr)
 os.chdir("..")
 shutil.move('src_updater/dist/', installDir + "updater-lib/")
 
 os.chdir("src")
-subprocess.call("cxfreeze.py item_tracker.py --base-name=Win32GUI --target-dir dist --icon ../mind.ico", shell=True, stdout=sys.stdout, stderr=sys.stderr)
+subprocess.call(localpy + " cxfreeze.py item_tracker.py --base-name=Win32GUI --target-dir dist --icon ../mind.ico", shell=False, stdout=sys.stdout, stderr=sys.stderr)
 os.chdir("..")
 shutil.copy('src/options.ico', 'src/dist/options.ico')
 shutil.move('src/dist/', installDir + 'tracker-lib/')
@@ -46,3 +51,5 @@ shutil.copy('LICENSE.txt', installDir)
 shutil.copy('README.md', installDir + 'README.txt')
 shutil.copy('version.txt', installDir)
 shutil.make_archive("target/" + installName + "-" + version, "zip", 'target', installName + "/")
+
+exit()
